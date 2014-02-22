0201c302d5167ef1754fb70a98776d83ade081da
Sergey Ovechkin <me@sovechkin.com>
2014-02-22 16:31:59 +0400

    Добавлены схемы для базы данных

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    xml2js   = require('xml2js'),
+    rest     = require('restler'),
+    crypto   = require('crypto'),
+    mongoose = require('mongoose'),
+    Schema   = mongoose.Schema,
+    moment   = require('moment')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,3 +13,46 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+
+//mongodb
+
+mongoose.connect('mongodb://mongodb.fr1.server.sovechkin.com/intimmarket');
+
+var UsersSchema = new Schema();
+
+UsersSchema.add({
+    partnerid   : { type: Number, index: true },
+    email       : { type: String, lowercase: true },
+    pass        : String,
+    admin       : Boolean,
+    created_at  : Date,
+    updated_at  : Date,
+    enabled     : Boolean
+});
+
+var OrdersSchema = new Schema();
+
+OrdersSchema.add({
+    ordersid    : { type: Number, index: true },
+    sum         : Number,
+    quantity    : Number,
+    status      : String,
+    items       : [ItemsSchema],
+    created_at  : Date,
+    updated_at  : Date,
+    enabled     : Boolean
+});
+
+var ItemsSchema = new Schema();
+
+ItemsSchema.add({
+    insales_id  : { type: Number, index: true },
+    url         : { type: String, lowercase: true },
+    pass        : String,
+    created_at  : Date,
+    updated_at  : Date,
+    enabled     : Boolean
+});
+
+var User = mongoose.model('User', UsersSchema);
+var Order = mongoose.model('Order', OrdersSchema);
