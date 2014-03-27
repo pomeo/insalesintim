faaae39d86e953d0e5440c627d54d169a31d457a
Sergey Ovechkin <me@sovechkin.com>
2014-03-27 16:31:59 +0400

    т.к. partner.js используется только при локальных тестах, возвращены локальные адреса

--- public/js/partner.js
+++ public/js/partner.js
@@ -2,10 +2,10 @@ $(function() {
   var results = new RegExp('[\?&]partner=([^&#]*)').exec(window.location.href);
   if (results !== null) {
     if ($.cookie('partner') == undefined) {
-      $.get('http://test2.sovechkin.com/unique/' + results[1]);
+      $.get('http://localhost:3000/unique/' + results[1]);
       $.cookie('partner', results[1], { expires: 180, path: '/' });
     } else {
-      $.get('http://test2.sovechkin.com/unique/' + $.cookie('partner'));
+      $.get('http://localhost:3000/unique/' + $.cookie('partner'));
     }
   }
 });
