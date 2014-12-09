be7597b1f89f03f91f45516beccf877d31077379
Sergey Ovechkin <me@sovechkin.com>
2014-12-09 16:31:59 +0400

    capistrano с сервера на DO

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,12 +8,19 @@ set :application, "test2.sovechkin.com"
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/insalesintim.git"
-set :supervisord_start_group, "test"
-set :supervisord_stop_group, "test"
+set :deploy_to,   "/home/ubuntu/www/intim"
+set :supervisord_start_group, "intim"
+set :supervisord_stop_group, "intim"
 #========================
 #ROLES
 #========================
-set  :gateway,    "#{application}"   # main server
-role :app,        "ubuntu@10.3.42.2" # lxc container
- 
-after "deploy:create_symlink", "deploy:npm_install", "deploy:restart"
+role :app,        "ubuntu@#{application}"
+
+namespace :deploy do
+  desc "Change node.js port"
+  task :chg_port do
+    run "sed -i 's/3000/8000/g' #{current_path}/app.js"
+  end
+end
+
+after "deploy:create_symlink", "deploy:npm_install", "deploy:npm_install", "deploy:restart"
